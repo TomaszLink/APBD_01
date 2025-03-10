03c2c910581644f9e0350d872a9945d443d793c7
Tomasz Link <tmszlink@gmail.com>
2025-03-10 19:47:54 +0100

    Added max method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,9 @@
 double result = average(numbers);
 Console.WriteLine(result);
 
+int[] numbers2 = { 1, 2, 3, 4, 25, 6, 7, 8, 9 };
+Console.WriteLine(max(numbers2));
+
 
 static double average(double[] numbers)
 {
@@ -11,4 +14,17 @@ static double average(double[] numbers)
         sum += numbers[i];
     }
     return sum / numbers.Length;
+}
+
+
+static int max(int[] numbers)
+{
+    int max = numbers[0];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if(numbers[i] > max)
+            max = numbers[i];
+    }
+
+    return max;
 }
