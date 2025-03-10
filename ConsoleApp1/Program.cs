a8d2336e01a9e9ddf5d993edb45509d4964d907b
Tomasz Link <tmszlink@gmail.com>
2025-03-10 19:45:08 +0100

    Added average method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿double[] numbers = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+double result = average(numbers);
+Console.WriteLine(result);
+
+
+static double average(double[] numbers)
+{
+    double sum = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        sum += numbers[i];
+    }
+    return sum / numbers.Length;
+}
